540817233bcc2b204163a81001d2ba3cb5e6c4a6
misshanya <mishanyadev@gmail.com>
2025-04-26 22:44:29 +0300

    feat: user handler test: get me

---
+++ internal/api/handler/user_test.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/misshanya/mitter/internal/models"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type mockUserService struct{}
+
+func (s *mockUserService) GetUser(ctx context.Context, id uuid.UUID) (*models.User, *models.HTTPError) {
+	_ = ctx
+	return &models.User{
+		ID:             id,
+		Login:          "testuser",
+		Name:           "Test User",
+		HashedPassword: "abracadabra",
+	}, nil
+}
+
+func TestUserHandler_GetMe(t *testing.T) {
+	e := echo.New()
+
+	handler := NewUserHandler(&mockUserService{})
+
+	g := e.Group("/api/v1/user")
+	handler.Routes(g)
+
+	// Create request
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/user/", nil)
+	rec := httptest.NewRecorder()
+
+	ctx := e.NewContext(req, rec)
+	ctx.Set("userID", uuid.MustParse("b096376a-5fa9-4130-907a-709c67008a65"))
+
+	if assert.NoError(t, handler.GetMe(ctx)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Contains(t, rec.Body.String(), "testuser")
+		assert.Contains(t, rec.Body.String(), "Test User")
+	}
+}
