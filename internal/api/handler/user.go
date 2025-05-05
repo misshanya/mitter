9c7c34f73c279232d4292af3cccf16d020fef5f7
misshanya <mishanyadev@gmail.com>
2025-05-05 17:57:22 +0300

    feat: users followers handlers

--- internal/api/handler/user.go
+++ internal/api/handler/user.go
@@ -15,6 +15,11 @@ type userService interface {
 	DeleteUser(ctx context.Context, id uuid.UUID) *models.HTTPError
 
 	UpdateUser(ctx context.Context, id uuid.UUID, user *models.UserUpdate) *models.HTTPError
+
+	FollowUser(ctx context.Context, followerID uuid.UUID, followeeID uuid.UUID) *models.HTTPError
+	UnfollowUser(ctx context.Context, followerID uuid.UUID, followeeID uuid.UUID) *models.HTTPError
+	GetUserFollows(ctx context.Context, followerID uuid.UUID) ([]*models.User, *models.HTTPError)
+	GetUserFollowers(ctx context.Context, followeeID uuid.UUID) ([]*models.User, *models.HTTPError)
 }
 
 type UserHandler struct {
@@ -33,6 +38,11 @@ func (h *UserHandler) Routes(group *echo.Group) {
 	group.GET("", h.getMe)
 	group.DELETE("", h.deleteUser)
 	group.PATCH("", h.updateUser)
+
+	group.POST("/:id/follow", h.followUser)
+	group.DELETE("/:id/follow", h.unfollowUser)
+	group.GET("/follows", h.getMyFollows)
+	group.GET("/followers", h.getMyFollowers)
 }
 
 // getMe godoc
@@ -131,4 +141,132 @@ func (h *UserHandler) updateUser(c echo.Context) error {
 	}
 
 	return c.NoContent(http.StatusOK)
+}
+
+// followUser godoc
+//
+//	@Tags		User
+//	@Summary	Follow user
+//	@Security	Bearer
+//	@Param		Authorization	header	string	true	"access token 'Bearer {token}'"
+//	@Param		id				path	string	true	"ID of user to follow to"
+//	@Success	200
+//	@Failure	400	{object}	dto.HTTPError
+//	@Failure	401	{object}	dto.HTTPError
+//	@Failure	500	{object}	dto.HTTPError
+//	@Router		/user/{id}/follow [post]
+func (h *UserHandler) followUser(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	userID := c.Get("userID").(uuid.UUID)
+
+	followeeIDStr := c.Param("id")
+	followeeID, err := uuid.Parse(followeeIDStr)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, dto.HTTPError{Message: err.Error()})
+	}
+
+	httpErr := h.service.FollowUser(ctx, userID, followeeID)
+	if httpErr != nil {
+		return c.JSON(httpErr.Code, dto.HTTPError{Message: httpErr.Message})
+	}
+
+	return c.NoContent(http.StatusOK)
+}
+
+// unfollowUser godoc
+//
+//	@Tags		User
+//	@Summary	Unfollow user
+//	@Security	Bearer
+//	@Param		Authorization	header	string	true	"access token 'Bearer {token}'"
+//	@Param		id				path	string	true	"ID of user to unfollow to"
+//	@Success	204
+//	@Failure	400	{object}	dto.HTTPError
+//	@Failure	401	{object}	dto.HTTPError
+//	@Failure	500	{object}	dto.HTTPError
+//	@Router		/user/{id}/follow [delete]
+func (h *UserHandler) unfollowUser(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	userID := c.Get("userID").(uuid.UUID)
+
+	followeeIDStr := c.Param("id")
+	followeeID, err := uuid.Parse(followeeIDStr)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, dto.HTTPError{Message: err.Error()})
+	}
+
+	httpErr := h.service.UnfollowUser(ctx, userID, followeeID)
+	if httpErr != nil {
+		return c.JSON(httpErr.Code, dto.HTTPError{Message: httpErr.Message})
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+// getMyFollows godoc
+//
+//	@Tags		User
+//	@Summary	Get My Follows
+//	@Security	Bearer
+//	@Param		Authorization	header		string	true	"access token 'Bearer {token}'"
+//	@Success	200				{object}	[]dto.UserResponse
+//	@Failure	400				{object}	dto.HTTPError
+//	@Failure	401				{object}	dto.HTTPError
+//	@Failure	500				{object}	dto.HTTPError
+//	@Router		/user/follows [get]
+func (h *UserHandler) getMyFollows(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	userID := c.Get("userID").(uuid.UUID)
+
+	users, httpErr := h.service.GetUserFollows(ctx, userID)
+	if httpErr != nil {
+		return c.JSON(httpErr.Code, dto.HTTPError{Message: httpErr.Message})
+	}
+
+	resp := make([]dto.UserResponse, len(users))
+	for i, user := range users {
+		resp[i] = dto.UserResponse{
+			ID:    user.ID,
+			Login: user.Login,
+			Name:  user.Name,
+		}
+	}
+
+	return c.JSON(http.StatusOK, resp)
+}
+
+// getMyFollowers godoc
+//
+//	@Tags		User
+//	@Summary	Get My Followers
+//	@Security	Bearer
+//	@Param		Authorization	header		string	true	"access token 'Bearer {token}'"
+//	@Success	200				{object}	[]dto.UserResponse
+//	@Failure	400				{object}	dto.HTTPError
+//	@Failure	401				{object}	dto.HTTPError
+//	@Failure	500				{object}	dto.HTTPError
+//	@Router		/user/followers [get]
+func (h *UserHandler) getMyFollowers(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	userID := c.Get("userID").(uuid.UUID)
+
+	users, httpErr := h.service.GetUserFollowers(ctx, userID)
+	if httpErr != nil {
+		return c.JSON(httpErr.Code, dto.HTTPError{Message: httpErr.Message})
+	}
+
+	resp := make([]dto.UserResponse, len(users))
+	for i, user := range users {
+		resp[i] = dto.UserResponse{
+			ID:    user.ID,
+			Login: user.Login,
+			Name:  user.Name,
+		}
+	}
+
+	return c.JSON(http.StatusOK, resp)
 }
