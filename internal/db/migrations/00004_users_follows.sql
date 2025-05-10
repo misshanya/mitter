a1fac2c1f836ca9de64967e0a0617632ac7bd5f9
misshanya <mishanyadev@gmail.com>
2025-05-10 15:06:22 +0300

    migrations: add indexes for users_follows table

--- internal/db/migrations/00004_users_follows.sql
+++ internal/db/migrations/00004_users_follows.sql
@@ -5,9 +5,15 @@ CREATE TABLE IF NOT EXISTS users_follows (
     follower_id UUID NOT NULL REFERENCES users(id),
     followee_id UUID NOT NULL REFERENCES users(id)
 );
+
+CREATE INDEX IF NOT EXISTS idx_users_follows_follower_id ON users_follows(follower_id);
+CREATE INDEX IF NOT EXISTS idx_users_follows_followee_id ON users_follows(followee_id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP INDEX IF EXISTS idx_users_follows_follower_id;
+DROP INDEX IF EXISTS idx_users_follows_followee_id;
+
 DROP TABLE IF EXISTS users_follows;
 -- +goose StatementEnd
